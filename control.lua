e1279b3cb469d3e6be675b42a87a1e986c384cde
Alex <71722762+alexe0336@users.noreply.github.com>
2023-12-25 02:12:35 -0500

    Update control.lua

--- control.lua
+++ control.lua
@@ -1,6 +1,6 @@
 script.on_event(defines.events.on_entity_died, function(event)
     local entity = event.entity
-    if entity and entity.valid and entity.type == "turret" then
+    if entity and entity.valid and entity.type == "gun-turret" then
         local inventory = entity.get_inventory(defines.inventory.turret_ammo)
         if inventory then
             for name, count in pairs(inventory.get_contents()) do
