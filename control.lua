ec14152a8af54f90d09e6176cf25d2b8af358d06
Alex <71722762+alexespinoza007@users.noreply.github.com>
2023-12-25 01:56:00 -0500

    Created control.lua and updated info.json

---
+++ control.lua
@@ -0,0 +1,12 @@
+/* Control.lua */
+script.on_event(defines.events.on_entity_died, function(event)
+    local entity = event.entity
+    if entity and entity.valid and entity.type == "turret" then
+        local inventory = entity.get_inventory(defines.inventory.turret_ammo)
+        if inventory then
+            for name, count in pairs(inventory.get_contents()) do
+                entity.surface.spill_item_stack(entity.position, {name=name, count=count}, true)
+            end
+        end
+    end
+end)
